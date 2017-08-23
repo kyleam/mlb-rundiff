b2890b1cca2d213ba00fb4cc1acb56bd0114c85a
Kyle Meyer <kyle@kyleam.com>
2017-08-23 14:26:43 -0400

    rmd/setup.R: Use consistent spacing around "="

--- rmd/setup.R
+++ rmd/setup.R
@@ -1,7 +1,7 @@
-knitr::opts_chunk$set(message=FALSE,
-                      fig.align="center",
-                      out.width="70%",
-                      fig.width=6,
-                      fig.asp=0.618,
-                      comment=NA,
-                      cache=TRUE)
+knitr::opts_chunk$set(message = FALSE,
+                      fig.align = "center",
+                      out.width = "70%",
+                      fig.width = 6,
+                      fig.asp = 0.618,
+                      comment = NA,
+                      cache = TRUE)
