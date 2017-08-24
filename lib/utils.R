415b1547e1ffc4837c219489bca312a6c91a05d8
Kyle Meyer <kyle@kyleam.com>
2017-08-24 10:23:22 -0400

    count_wins: Make fewer assumptions
    This function hard codes things like the column name because it's just
    a convenience function for an operation that I was repeating on the
    gamelog data frames.  Still, there's no need to assume that the
    away_team and home_team column neighbor each other or that team_type
    contains only valid values.

--- lib/utils.R
+++ lib/utils.R
@@ -9,11 +9,12 @@ require(dplyr)
 count_wins <- function(log_df){
     log_df %>%
         mutate(home_win = home_runs_scored > away_runs_scored) %>%
-        gather(team_type, team, away_team:home_team) %>%
-        mutate(win = case_when(home_win & team_type == "home_team"  ~ TRUE,
-                               home_win ~ FALSE,
+        gather(team_type, team, away_team, home_team) %>%
+        mutate(win = case_when(team_type == "home_team" & home_win ~ TRUE,
+                               team_type == "away_team" & home_win  ~ FALSE,
                                team_type == "away_team" ~ TRUE,
-                               TRUE ~ FALSE)) %>%
+                               team_type == "home_team"  ~ FALSE,
+                               TRUE ~ NA)) %>%
         group_by(team) %>%
         summarise(n_wins = sum(win), n_games = n())
 }
