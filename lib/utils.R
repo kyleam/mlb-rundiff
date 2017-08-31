f8cfc0cc377db3caf839caedbe100bf02c2bc92a
Kyle Meyer <kyle@kyleam.com>
2017-08-31 11:43:45 -0400

    count_wins: Use .data pronoun

--- lib/utils.R
+++ lib/utils.R
@@ -9,15 +9,16 @@ require(tidyr)
 ##
 count_wins <- function(log_df){
     log_df %>%
-        mutate(home_win = home_runs_scored > away_runs_scored) %>%
+        mutate(home_win = .data$home_runs_scored > .data$away_runs_scored) %>%
         gather(team_type, team, away_team, home_team) %>%
-        mutate(win = case_when(team_type == "home_team" & home_win ~ TRUE,
-                               team_type == "away_team" & home_win  ~ FALSE,
-                               team_type == "away_team" ~ TRUE,
-                               team_type == "home_team"  ~ FALSE,
-                               TRUE ~ NA)) %>%
+        mutate(win = case_when(
+                   .data$team_type == "home_team" & .data$home_win ~ TRUE,
+                   .data$team_type == "away_team" & .data$home_win  ~ FALSE,
+                   .data$team_type == "away_team" ~ TRUE,
+                   .data$team_type == "home_team"  ~ FALSE,
+                   TRUE ~ NA)) %>%
         group_by(team) %>%
-        summarise(n_wins = sum(win), n_games = n())
+        summarise(n_wins = sum(.data$win), n_games = n())
 }
 
 source_as_list <- function(file){
